2194edf7f9f583573252171e7b203b5bd66a451e
Sean McGillis <slcidevice@yahoo.com>
2022-02-03 16:39:21 -0700

    implemented the logic to add items to the cart from the mealItem component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,20 @@
 import styles from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
-  console.log('props.id', props.id); //@DEBUG
+    const cartContext = useContext(CartContext);
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+      cartContext.addItem({
+        id: props.id,
+        name: props.name,
+        amount: amount,
+        price: props.price
+      })
+    };
   return (
     <li className={styles.meal}>
       <div>
@@ -12,7 +23,7 @@ const MealItem = (props) => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,13 @@
+import React from 'react';
+
 import styles from './Input.module.css';
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return <div className={styles.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
         {/* ...props.input will add all the key-value pairs inside the props.input object to the input element. This makes is really easy to use this input for any setup */}
-        <input {...props.input}/>
+        <input ref={ref}{...props.input}/>
     </div>
-};
+});
 
 export default Input;

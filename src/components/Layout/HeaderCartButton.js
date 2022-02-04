759baf0255c39824cbe49e339ba03b992075074b
Sean McGillis <slcidevice@yahoo.com>
2022-02-03 21:36:32 -0700

    implemented the logic to increase or decrease the amount of an item if it already is in the cart and to remove it from the cart if the amount gets below 1. Also animated the cart button when an item is added or removed.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [bumpButton, setBumpButton] = useState(false);
   const cartContext = useContext(CartContext);
 
   const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnStyles = `${styles.button} ${bumpButton ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setBumpButton(true);
+
+    const timer = setTimeout(() => {
+      setBumpButton(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
